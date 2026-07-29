3c552a790704cf0841141c96affdb312a26ff789
mohdabdullah2662135-beep <mohdabdullah2662135@gmail.com>
2026-07-29 22:47:22 +0530

    Update pizza_data.dart

--- app/lib/data/pizza_data.dart
+++ app/lib/data/pizza_data.dart
@@ -5,17 +5,20 @@ final List<Pizza> pizzas = [
     name: "Margherita Pizza",
     description: "Cheese and tomato classic pizza",
     price: 199,
+    image: "assets/images/margherita.png",
   ),
 
   Pizza(
     name: "Paneer Pizza",
     description: "Spicy paneer loaded pizza",
     price: 299,
+    image: "assets/images/paneer.png",
   ),
 
   Pizza(
     name: "Farmhouse Pizza",
     description: "Fresh vegetables with cheese",
     price: 349,
+    image: "assets/images/farmhouse.png",
   ),
 ];
