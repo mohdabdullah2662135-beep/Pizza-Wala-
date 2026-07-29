b77f75cd1a490e7f224010ecfa7ab52dd3e65d32
mohdabdullah2662135-beep <mohdabdullah2662135@gmail.com>
2026-07-29 22:46:10 +0530

    Update pizza_model.dart

--- app/lib/models/pizza_model.dart
+++ app/lib/models/pizza_model.dart
@@ -2,10 +2,12 @@ class Pizza {
   final String name;
   final String description;
   final double price;
+  final String image;
 
   Pizza({
     required this.name,
     required this.description,
     required this.price,
+    required this.image,
   });
 }
