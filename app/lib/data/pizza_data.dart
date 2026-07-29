cd4e6341d91d18f5cc2a80dc4176f7973abd4be7
mohdabdullah2662135-beep <mohdabdullah2662135@gmail.com>
2026-07-29 22:19:05 +0530

    Create lib/data/pizza_data.dart

---
+++ app/lib/data/pizza_data.dart
@@ -0,0 +1,21 @@
+import '../models/pizza_model.dart';
+
+final List<Pizza> pizzas = [
+  Pizza(
+    name: "Margherita Pizza",
+    description: "Cheese and tomato classic pizza",
+    price: 199,
+  ),
+
+  Pizza(
+    name: "Paneer Pizza",
+    description: "Spicy paneer loaded pizza",
+    price: 299,
+  ),
+
+  Pizza(
+    name: "Farmhouse Pizza",
+    description: "Fresh vegetables with cheese",
+    price: 349,
+  ),
+];
