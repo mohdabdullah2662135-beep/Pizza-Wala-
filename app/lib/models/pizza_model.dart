059b484b06736845dbae65e3b7fc4d08da8f1476
mohdabdullah2662135-beep <mohdabdullah2662135@gmail.com>
2026-07-29 22:17:25 +0530

    Create lib/models/pizza_model.dart

---
+++ app/lib/models/pizza_model.dart
@@ -0,0 +1,11 @@
+class Pizza {
+  final String name;
+  final String description;
+  final double price;
+
+  Pizza({
+    required this.name,
+    required this.description,
+    required this.price,
+  });
+}
