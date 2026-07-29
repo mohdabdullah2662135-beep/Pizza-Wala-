0091f52f166b9aba421466a94790b68ec3a05c74
mohdabdullah2662135-beep <mohdabdullah2662135@gmail.com>
2026-07-29 22:42:07 +0530

    Update cart_screen.dart

--- app/lib/screens/cart_screen.dart
+++ app/lib/screens/cart_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../services/cart_service.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
@@ -10,13 +11,41 @@ class CartScreen extends StatelessWidget {
         title: const Text("🛒 My Cart"),
       ),
 
-      body: const Center(
-        child: Text(
-          "Your cart is empty",
-          style: TextStyle(
-            fontSize: 24,
+      body: Column(
+        children: [
+
+          Expanded(
+            child: ListView.builder(
+              itemCount: CartService.cartItems.length,
+
+              itemBuilder: (context, index) {
+                final item = CartService.cartItems[index];
+
+                return ListTile(
+                  title: Text(item.name),
+
+                  subtitle: Text(
+                    "Quantity: ${item.quantity}",
+                  ),
+
+                  trailing: Text(
+                    "₹${item.price}",
+                  ),
+                );
+              },
+            ),
           ),
-        ),
+
+          Text(
+            "Total: ₹${CartService.getTotal()}",
+            style: const TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+
+          const SizedBox(height: 20),
+        ],
       ),
     );
   }
